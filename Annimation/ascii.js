5ad5f0513329db4314e231cd76ece5d02d9a06ab
ahmedelghannam1 <aelghannam@miu.edu>
2021-06-27 22:55:48 -0500

    play

--- Annimation/ascii.js
+++ Annimation/ascii.js
@@ -1,25 +1,35 @@
-window.onload=function(){
-    function splitFunc(){
-    return ANIMATIONS[document.getElementById("animation").value].split("=====");
-    }
-
-    function play(a){
-        document.getElementById("text-area").value=a; 
-    }
-    function playMotion(){
-   /*     var a =splitFunc();
-        for (var i=0; i<a.length;i++){
-          // play(a[i])
-            setInterval(play, 250,a[i]);
-
-        }
-     */   
-        document.getElementById("text-area").value="Ahmed";
-
-    }
-    playMotion();
-
-    //document.getElementById("start").onclick=function(){
-    //document.getElementById("text-area").value=ANIMATIONS[document.getElementById("animation").value];
-    }
+function allAnimation(){
+    document.getElementById("start").onclick=start;
+   
+ 
 }
+function start(){
+    playMotion();   
+}
+function play(a){
+    document.getElementById("text-area").value=a; 
+}
+function playMotion(){
+    var a =splitFunc();
+    for (var i=0; i<a.length;i++){
+      var timerplay=setInterval(play, 250,a[i]);
+      //cleartext();
+
+    }
+   
+}
+
+function cleartext(){
+    document.getElementById("text-area").value=""; 
+}
+
+
+
+
+function splitFunc(){
+    var x= ANIMATIONS[document.getElementById("animation").value].split("=====");
+    
+    return x;
+}
+
+window.onload=allAnimation;
