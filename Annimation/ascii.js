77f461b5bfe1aa3606d04e025219ba217cea54cb
ahmedelghannam1 <aelghannam@miu.edu>
2021-06-27 22:11:12 -0500

    save

--- Annimation/ascii.js
+++ Annimation/ascii.js
@@ -1,5 +1,24 @@
 window.onload=function(){
+    function splitFunc(){
+    return ANIMATIONS[document.getElementById("animation").value].split("=====");
+    }
+
+    function play(a){
+        document.getElementById("text-area").value=a; 
+    }
+    function playMotion(){
+        var a =splitFunc();
+        for (var i=0; i<a.length;i++){
+            setInterval(() => {
+                document.getElementById("text-area").value=a[i];    
+            }, 250);
+
+        }
+
+
+    }
+
     document.getElementById("start").onclick=function(){
-        alert("you clicked start")
+    document.getElementById("text-area").value=ANIMATIONS[document.getElementById("animation").value];
     }
 }
