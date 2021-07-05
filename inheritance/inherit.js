1b36aabee185ab6d1a0cf4e9d2d6626d42c1b8ef
ahmedelghannam1 <aelghannam@miu.edu>
2021-07-05 02:43:08 -0500

    test

--- inheritance/inherit.js
+++ inheritance/inherit.js
@@ -8,7 +8,56 @@ console.log("This house is not nice!".filter('not'));
 Output: "This house is nice!"
  * 
  */
-Str=" I don't like Maharishi, University is not giving me chance to enjoy life."
-bannedstr=["don't", "not"];
-goodfunction=str.filter((acc,curr)=>{if (!bannedstr.includes(curr)) return curr;});
-console.log(goodfunction);
+function GoodWords(){
+ 
+
+}
+GoodWords.prototype.filterme= function  (strorg,filt) {
+        var strarry=strorg.split(" ");
+        var temp= strarry. filter((curr)=> (!filt.includes(curr)) ). reduce((accum,curr)=>accum.concat(curr," "),"");
+        return temp;
+        
+   
+    };
+const srcstr= new GoodWords();
+ 
+    
+
+var str="I don't like Maharishi, University is not giving me chance to enjoy life.";
+var bannedstr=["don't", "not"];
+
+
+console.log( srcstr.filterme(str,bannedstr));
+
+
+
+/**
+ * Exercise 2:
+Write a BubbleSort algorithm on the Array object. Bubble sort is a simple sorting algorithm
+that works by repeatedly stepping through the list to be sorted,
+Example:[6,4,0, 3,-2,1].bubbleSort();
+Output : [-2, 0, 1, 3, 4, 6]
+ * 
+ * 
+ * 
+ * 
+ */
+
+function BubbleSort(arr){
+    
+    return{bubleSort:
+    function (){
+        for (let i=0;i<arr.length;i++)
+            for (let j=i+1;j<arr.length;j++)
+           if(i<arr.length-1) 
+            if(arr[i]>arr[j])
+            {
+                let temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+    return arr;
+    }};
+}
+var bs= new BubbleSort([4,3,5,1,-2,1]);
+console.log(bs.bubleSort());
