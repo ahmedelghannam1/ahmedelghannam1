1be94982817caf9f59bfad36cd33abf64b3b7ec2
ahmedelghannam1 <aelghannam@miu.edu>
2021-07-06 05:59:11 -0500

    test

--- classes/test.js
+++ classes/test.js
@@ -5,29 +5,36 @@ describe("Accounts", function() {
     function makeTest(x) {
         var acc= new Account(10001);
         let bal=acc.getBalance();
-        var sav=new SavingsAccount(2.5);
-        
+        var sav=new SavingsAccount(10002,2.5);
+        let savBal=sav.getBalance();
+        sav.deposit(1000);
+        let interest=sav.getInterest();
       let expected = x+bal;
       acc.deposit(x);
       bal =acc.getBalance();
       let wth= acc.withdraw(5);
       
-      it(`${x} to be deposited to ${0} ${expected}`, function() {
+      it(`the deposit function make ${x} to be deposited to ${0} ${expected}`, function() {
         assert.equal(bal, expected);
       });
-      it(` to  get balance to ${bal} ${expected}`, function() {
+      it(`the get balance of  ${bal}  if ${expected}`, function() {
         assert.equal(bal, expected);
       });
-      it(`${5} to withdraw from  balance of  ${bal} ${bal-5}`, function() {
+      it(`the withdraw function of ${5} from  balance of  ${bal} ${bal-5}`, function() {
         assert.equal(bal, expected);
       });
-      
+      expected=savBal*interest;
+      let intamount=sav.getInterestAmount();
+      it(`get interest of ${2.5}/100 from balance of  ${savBal} ${savBal*int}`, function() {
+        assert.equal(intAmount, expected);
+      });
+
       
     }
   
-    for (let x = 1; x <= 1; x++) {
+    
       makeTest(100);
-    }
+    
   
   });
 
