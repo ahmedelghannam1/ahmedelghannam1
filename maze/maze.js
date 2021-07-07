4ac7d3a3c867aaee70e759a82c111b1d664cb77d
ahmedelghannam1 <aelghannam@miu.edu>
2021-07-07 15:17:25 -0500

    hover

--- maze/maze.js
+++ maze/maze.js
@@ -1,11 +1,15 @@
 $(function() {
+    var wallHit=0;
+    var startVisited=0;
+
     
     function youlose (){
         if ($(this).attr("id")==="boundary1")
         $(this).addClass("youlose");
         else
         $(".boundary").addClass("youlose");
-        alert("you lose");    
+        alert("you lose");  
+        wallHit=1;  
         }
         
         $("#boundary1").mouseenter(youlose)
@@ -14,11 +18,23 @@ $(function() {
   
 
     const strt= function () {
-       location.reload();
-        
+       wallHit=0;
+      // alert($("#start").hoverdir()+"ahmed");
+        $(".boundary").removeClass("youlose");
+        startVisited=1;
 
     }
-    $("#start").mouseover(strt);
+    $("#start").on("click",()=>{strt();});
+    const end= function () {
+       
+        if (wallHit===0 && startVisited===1)
+        {
+            alert("you win");
+            startVisited=0;
+        }
+
+    }
+    $("#end").mouseover(end);
     
 
 });
