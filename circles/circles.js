1d46838fd06fc114aa6d5769d648add44b711188
ahmedelghannam1 <aelghannam@miu.edu>
2021-07-07 07:41:14 -0500

    st

--- circles/circles.js
+++ circles/circles.js
@@ -1,14 +1,32 @@
 
 $(document).ready(function(){
 
-  const rem= function removeElement(){
-        $("#1").remove();
-        clearInterval(this._timer);
+  var timer=null;
+  const rem= function (){
+        $("#1").hide();
+        clearInterval(timer);
+        timer=null;
+    }
+    const shw=function(id){
+        clearInterval(timer);
+        setsz(1,$("#width").val());
+        $("#"+id).toggle('show');
+        let delay= $("#interval").val();
+        let growth= $("#grossAmount").val();
+        
+        timer=setInterval(()=>{enlargeCircle(growth)},delay);
+
+
     }
   /* 
    document.getElementById("#1").onclick=removeElement;
     var timer=null;*/
+   const setsz= function setsize(id,size){
+       
+        $("#" + id +"").css("height", size+"px"); 
+        $("#" + id +"").css("width", size+"px");  
 
+    }
     function enlargeCircle(delta) {
        var id=1;
         $("#" + id +"").css("height", function(idx, old) {
@@ -22,7 +40,8 @@ $(document).ready(function(){
 
     }
     timer=setInterval(()=>{enlargeCircle(10)},250);
-    $(document).on( 'click', 'div', rem);
+    $(document).on( 'click', '#1',()=>{ rem(1)});
+    $(document).on( 'click', '#start',()=>{ shw(1)});
      // document.getElementById(1).onclick=removeElement; 
 });
 //window.onload=animation;
