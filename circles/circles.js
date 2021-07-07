48da69a1cb4a4b7d937a0470049017e9950e6df3
ahmedelghannam1 <aelghannam@miu.edu>
2021-07-07 09:55:00 -0500

    final

--- circles/circles.js
+++ circles/circles.js
@@ -16,14 +16,10 @@ $(document).ready(function(){
         
         for ( i=1;i<num;i++)
         {
-           
-            
             $("<body>").prepend($("<div>", {
                 "class": "circle",
                 
                }));
-               
-          
             
             
        }
